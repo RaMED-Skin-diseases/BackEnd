0b2155ca054eb70fa074fb42c6eff556c4eb2f41
Ahmed Hafez <ahmed.hafez307@gmail.com>
2025-03-28 16:44:35 +0200

    updated

--- skin_diseases/urls.py
+++ skin_diseases/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('community/', include('community.urls')),
-    path('detect/', include('detect.urls')),
+    #path('detect/', include('detect.urls')),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
